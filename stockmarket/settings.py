953064f9100566c59b3067435d9c69d2a9c217e6
Dev Mehta <devbmehta04@gmail.com>
2021-08-19 18:20:40 +0530

    Configuring Staticfiles

--- stockmarket/settings.py
+++ stockmarket/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [
 # 	BASE_DIR / 'static',
 # ]
