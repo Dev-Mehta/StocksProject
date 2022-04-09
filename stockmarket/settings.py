1a774ff4f737c53fd0d3211c2f5c723ef1128920
Dev Mehta <55938019+Dev-Mehta@users.noreply.github.com>
2022-04-09 13:43:42 +0530

    update setttings for production

--- stockmarket/settings.py
+++ stockmarket/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [
-	BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+# 	BASE_DIR / 'static',
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
